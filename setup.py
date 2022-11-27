7c8b265bfe0d7f49e0e039c38976480893f4ff49
Labarriere Luc <luc.labarriere@univ-lille.fr>
2022-11-27 18:23:11 +0100

    Updated conda env and pip requirements

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setuptools.setup(
     name="PyGJK",
     packages=setuptools.find_packages(),
     install_requires=[
+        "numpy",
+        "pyqt6",
+        "scipy",
+        "pyqtgraph",
     ]
 )
